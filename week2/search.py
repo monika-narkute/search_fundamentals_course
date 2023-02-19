a1678425c8999faf798c61fe7c9a379c9c88f079
Monika Narkutė <monika.narkute@vinted.com>
2023-02-19 11:50:28 +0000

    autocomplete & instant search

--- week2/search.py
+++ week2/search.py
@@ -57,15 +57,31 @@ def process_filters(filters_input):
 
 @bp.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    opensearch = get_opensearch()
     results = {}
     if request.method == 'GET':  # a query has been submitted
         prefix = request.args.get("prefix")
         print(f"Prefix: {prefix}")
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
-            ##### W2, L3, S1
-            search_response = None
-            print("TODO: implement autocomplete AND instant search")
+            autocompleteQueryBody = {
+                "suggest": {
+                    "autocomplete": {
+                        "prefix": prefix,
+                        "completion": {
+                            "field": "suggest",
+                            "skip_duplicates": "true"
+                        }
+                    }
+                }
+            }
+            queryIndex = ""
+            if type == "queries":
+                queryIndex = "bbuy_queries"
+            else:
+                queryIndex = "bbuy_products"
+
+            search_response = opensearch.search(autocompleteQueryBody, queryIndex)
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
     print(f"Results: {results}")
@@ -105,9 +121,8 @@ def query():
             explain = True
 
         query_obj = qu.create_query(user_query,  [], sort, sortDir, size=20)  # We moved create_query to a utility class so we could use it elsewhere.
-        ##### W2, L1, S2
-
-        ##### W2, L2, S2
+        qu.add_click_priors(query_obj, user_query, prior_clicks)
+        qu.add_spelling_suggestions(query_obj, user_query)
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -120,10 +135,8 @@ def query():
         if filters_input:
             (filters, display_filters, applied_filters) = process_filters(filters_input)
         query_obj = qu.create_query(user_query,  filters, sort, sortDir, size=20)
-        #### W2, L1, S2
-
-        ##### W2, L2, S2
-
+        qu.add_click_priors(query_obj, user_query, prior_clicks)
+        qu.add_spelling_suggestions(query_obj, user_query)
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
 
--- week2/utilities/query_utils.py
+++ week2/utilities/query_utils.py
@@ -158,19 +158,31 @@ def create_query(user_query, filters, sort="_score", sortDir="desc", size=10, in
 ##########
 # Give a user query from the UI and the query object we've built so far, adding in spelling suggestions
 def add_spelling_suggestions(query_obj, user_query):
-    #### W2, L2, S1
-    print("TODO: IMPLEMENT ME")
-    #query_obj["suggest"] = {
-    #    "text": user_query,
-    #    "phrase_suggest": {
-
-    #    },
-    #    "term_suggest": {
-
-    #    }
-    #}
-
-
+    query_obj["suggest"] = {
+        "text": user_query,
+        "phrase_suggest": {
+            "phrase": {
+                "field": "suggest.trigram",
+                "direct_generator": [{
+                    "field": "suggest.trigram",
+                    "min_word_length": 2,
+                    "suggest_mode": "popular"
+                }],
+                    "highlight": {
+                      "pre_tag": "<em>",
+                      "post_tag": "</em>"
+                    }
+            }
+        },
+        "term_suggest": {
+            "term": {
+                "field": "suggest.text",
+                "min_word_length": 3,
+                "suggest_mode": "popular"
+            }
+        }
+    }
+    
 # Given the user query from the UI, the query object we've built so far and a Pandas data GroupBy data frame,
 # construct and add a query that consists of the ids from the items that were clicked on by users for that query
 # priors_gb (loaded in __init__.py) is grouped on query and has a Series of SKUs/doc ids for every document that was cliecked on for this query
